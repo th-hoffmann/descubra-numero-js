c2846d1b26b8f1ef0fda894cf33094275d3ec259
Thiago <th.hoffmann@protonmail.com>
2025-10-04 01:01:54 -0300

    :sparkles: feat:Numero secreto passa a ser dinamicamente gerado usando números pseudo-aleatórios

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 alert("Olá! Seja Bem Vindo ao Jogo!");
-let numeroSecreto = 42;
-console.log(numeroSecreto);
-let palpite = prompt("Digite um número entre 0 e 100:");
+let numeroLimite = 100;
+let numeroSecreto = parseInt(Math.random() * numeroLimite + 1);
+let palpite;
+let tentativas = 1;
 
-// Se palpite for igual ao número secreto, exiba uma mensagem de parabéns
-if (palpite == numeroSecreto) {
-    alert(`Parabens! Você acertou o número secreto! ${numeroSecreto}`);
-} else {
-    alert("Que pena! Você errou o número secreto. :( ");
+//Enquanto o palpite não for um número válido, continue pedindo um novo palpite
+while (palpite != numeroSecreto) {
+    palpite = prompt(`Digite um número entre 1 e ${numeroLimite}`);
+    // Se palpite for igual ao número secreto, exiba uma mensagem de parabéns
+    if (palpite == numeroSecreto) {
+        break;
+    } else {
+        if (palpite > numeroSecreto) {
+            alert(`Você errou! O número secreto é menor que ${palpite}`);
+        } else {
+            alert(`Você errou! O número secreto é maior que ${palpite}`);
+        }
+        // Incrementa o número de tentativas
+        tentativas++;
+    }
 }
+
+let mensagemFinal = tentativas > 1 ? "tentativas" : "tentativa";
+// Exiba uma mensagem de parabéns com o número de tentativas
+alert(`Parabens! Você acertou o número secreto: ${numeroSecreto} em ${tentativas} ${mensagemFinal}.`);
+
